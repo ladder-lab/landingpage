11eeb481e5ed49215bd4cfc907054d6f0fa0d305
cw286 <chiwei@ladder.top>
2022-07-03 11:52:35 +0800

    mobile Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,11 @@
-import { AppBar, Box, Button, styled } from '@mui/material'
+import { AppBar, Box, Button, styled, IconButton } from '@mui/material'
 import { ReactComponent as Ladder } from 'assets/svg/ladder.svg'
+import { ReactComponent as LadderSm } from 'assets/svg/ladder-sm.svg'
 import { ReactComponent as ArrowUpRight } from 'assets/svg/arrow-up-right.svg'
+import { ReactComponent as MenuIcon } from 'assets/svg/menu.svg'
 import { routes } from 'constants/routes'
 import { NavLink } from 'react-router-dom'
+import useBreakpoint from 'hooks/useBreakpoint'
 
 const StyledAppBar = styled(AppBar)({
   background: 'transparent',
@@ -11,6 +14,14 @@ const StyledAppBar = styled(AppBar)({
   flexDirection: 'row',
   height: 109,
   boxShadow: 'none'
+})
+
+const StyledMobileAppBar = styled(AppBar)({
+  background: 'transparent',
+  borderBottom: '1px solid #E8E8E8',
+  height: 88,
+  boxShadow: 'none',
+  width: '100%'
 })
 
 const StyledNavLink = styled(NavLink)({
@@ -22,22 +33,15 @@ const StyledNavLink = styled(NavLink)({
 })
 
 export default function Header() {
+  const isDownSm = useBreakpoint('sm')
+
+  if (isDownSm) {
+    return mobileHeader
+  }
+
   return (
     <StyledAppBar position="absolute">
-      <Box
-        sx={{
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRight: '1px solid #E8E8E8',
-          padding: '37px 45px'
-        }}
-      >
-        <NavLink to={routes.home}>
-          <Ladder />
-        </NavLink>
-      </Box>
+      <BrandLogo />
       <Box
         sx={{
           width: '100%',
@@ -52,20 +56,92 @@ export default function Header() {
         <StyledNavLink to={routes.home}>Docs</StyledNavLink>
         <StyledNavLink to={routes.community}>Community</StyledNavLink>
       </Box>
-      <Button
-        variant="contained"
-        sx={{
-          width: 230,
-          borderRadius: 0,
-          background: 'linear-gradient(0deg, #FE1A5E, #FE1A5E), #D9D9D9;',
-          boxShadow: 'none'
-        }}
-      >
-        <Box display="flex" alignItems="center" gap={12}>
-          LAUNCH APP
-          <ArrowUpRight />
-        </Box>
-      </Button>
+      <LaunchApp />
     </StyledAppBar>
+  )
+}
+
+const mobileHeader = (
+  <StyledMobileAppBar position="absolute">
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        height: '100%'
+      }}
+    >
+      <BrandLogo />
+      <LaunchApp />
+      <MenuBtn />
+    </Box>
+  </StyledMobileAppBar>
+)
+
+function BrandLogo() {
+  const isDownSm = useBreakpoint('sm')
+
+  return (
+    <Box
+      width={80}
+      sx={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRight: '1px solid #E8E8E8',
+        padding: '37px 45px',
+        width: {
+          xs: 80,
+          md: 230
+        },
+        background: {
+          xs: '#8A7392',
+          md: 'transparent'
+        }
+      }}
+    >
+      <NavLink to={routes.home}>{isDownSm ? <LadderSm /> : <Ladder />}</NavLink>
+    </Box>
+  )
+}
+
+function LaunchApp() {
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        height: '100%',
+        width: {
+          md: 230
+        },
+        borderRadius: 0,
+        background: 'linear-gradient(0deg, #FE1A5E, #FE1A5E), #D9D9D9;',
+        boxShadow: 'none',
+        flexGrow: 1
+      }}
+      endIcon={<ArrowUpRight />}
+    >
+      LAUNCH APP
+    </Button>
+  )
+}
+
+function MenuBtn() {
+  return (
+    <Box
+      sx={{
+        borderLeft: '1px solid #E8E8E8',
+        width: 80,
+        height: '100%',
+        background: '#3BBDAC',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <IconButton>
+        <MenuIcon />
+      </IconButton>
+    </Box>
   )
 }
