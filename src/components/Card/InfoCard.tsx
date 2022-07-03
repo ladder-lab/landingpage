4e1cb03a36486249aac9e6dfd5310cbf2185053e
cw286 <chiwei@ladder.top>
2022-07-03 15:36:46 +0800

    infocard rwd

--- src/components/Card/InfoCard.tsx
+++ src/components/Card/InfoCard.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react'
 import { Box, Typography } from '@mui/material'
 import { ReactComponent as PointArray } from 'assets/svg/point-array.svg'
+import useBreakpoint from 'hooks/useBreakpoint'
 
 export default function InfoCard({
   text,
@@ -15,6 +16,8 @@ export default function InfoCard({
   width?: number | string
   height?: number | string
 }) {
+  const isDownMd = useBreakpoint('md')
+
   const borderRadius = useMemo(() => {
     switch (type) {
       case 'upper-left':
@@ -52,7 +55,10 @@ export default function InfoCard({
         background: 'rgba(255, 255, 255, 0.26)',
         overflow: 'hidden',
         width: width || '100%',
-        height: height || 202,
+        height: {
+          xs: height || 185,
+          md: height || 220
+        },
         display: 'flex',
         flexDirection: ['upper-left', 'upper-right'].includes(type) ? 'column' : 'column-reverse'
       }}
@@ -64,15 +70,13 @@ export default function InfoCard({
         flexDirection={['upper-left', 'lower-left', 'lower'].includes(type) ? 'row' : 'row-reverse'}
       >
         {type === 'lower' && (
-          <Typography fontSize={22} fontWeight={400} pl={20}>
-            {text}
-          </Typography>
+          <Typography sx={{ fontSize: { xs: 16, md: 22 }, fontWeight: 400, pl: 20 }}>{text}</Typography>
         )}
         <Box
           sx={{
             background: 'rgba(255, 255, 255, 0.26)',
-            width: 90,
-            height: 90,
+            width: { xs: 75, md: 90 },
+            height: { xs: 72, md: 90 },
             borderRadius: iconBorderRadius,
             display: 'flex',
             alignItems: 'center',
@@ -81,14 +85,16 @@ export default function InfoCard({
         >
           {icon}
         </Box>
-        {type !== 'lower' && <PointArray style={{ margin: 20 }} />}
+        {type !== 'lower' && (
+          <PointArray
+            style={{ margin: isDownMd ? -12 : '0 20px', transform: isDownMd ? 'rotate(90deg) scale(0.6)' : 'none' }}
+          />
+        )}
       </Box>
 
       {type !== 'lower' && (
-        <Box padding="24px 20px 36px">
-          <Typography fontSize={22} fontWeight={400}>
-            {text}
-          </Typography>
+        <Box sx={{ padding: { xs: '28px 16px 28px', md: '24px 20px 36px' } }}>
+          <Typography sx={{ fontSize: { xs: 16, md: 22 }, fontWeight: 400 }}>{text}</Typography>
         </Box>
       )}
     </Box>
